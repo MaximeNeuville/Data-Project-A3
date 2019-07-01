0a7a456b996616f38e42ab27c7ad2b538e337c56
Guillaume <guyoms974@hotmail.fr>
2019-07-01 13:50:23 +0200

    Almost final version datasetGen

--- src/DataSetGenerator.py
+++ src/DataSetGenerator.py
@@ -5,56 +5,57 @@ import random
 def random_symmetric_matrix(n):
 
     # TO DO
-    # if int(n) < 2: 
-    #     random_symmetric_matrix(input("Error, please enter at least 2 cities"))
+    if int(n) < 2: 
+        random_symmetric_matrix(input("Error, please enter at least 2 cities : "))
+        exit()
 
     maxTime = 12
     # cree les valeurs random necessaire pour remplir la matrice finale 
-    # (on genere seulement le nb de valeur necessaire a un triange)
-    R = np.random.randint(0, maxTime, size = int((n*(n-1)/2)))
+    # (on genere seulement le nb de valeur necessaire a un triange superieur)
+    R = np.random.randint(1, maxTime, size = int((n*(n-1)/2)))
     # cree une matrice remplie de 0
     P = np.zeros((n,n))
 
     # 1 = partie top du triange, -1 partie bottom
     P[np.triu_indices(n, 1)] = R
 
-    # On prend la partie du triange bas de la matrice transposée
+    # On prend la partie du triange bas de la matrice transposee
     P[np.tril_indices(n, -1)] = P.T[np.tril_indices(n, -1)]
     
     print("Matrice avant")
     print(P)
 
 
-    for i in range(0,n):
-        # on teste si toutes les villes ont au moins une connexion
-        if int(P[i].sum()) <= 0:
-            print("ERROR line : ")
-            print(i)
-            print(int(P[i].sum()))
-            randomX = np.random.randint(0,n-1)
-            print("randomX")
-            print(randomX)
+    # for i in range(0,n):
+    #     # on teste si toutes les villes ont au moins une connexion
+    #     if int(P[i].sum()) <= 0:
+    #         # print("ERROR line : ")
+    #         # print(i)
+    #         # print(int(P[i].sum()))
+    #         randomX = np.random.randint(0,n-1)
+    #         # print("randomX")
+    #         # print(randomX)
 
-            randomTime = np.random.randint(1,maxTime)
+    #         randomTime = np.random.randint(1,maxTime)
             
-            print("randomTime")
-            print(randomTime)
+    #         # print("randomTime")
+    #         # print(randomTime)
 
-            # On vérifie qu'ils ne sont pas égaux pour ne pas ajouter une valeur dans la diagonale
-            while randomX == i: 
-                randomX = np.random.randint(0,n-1)
-            P[i][randomX] = randomTime
-            P[randomX][i] = randomTime
+    #         # On verifie qu'ils ne sont pas egaux pour ne pas ajouter une valeur dans la diagonale
+    #         while randomX == i: 
+    #             randomX = np.random.randint(0,n-1)
+    #         P[i][randomX] = randomTime
+    #         P[randomX][i] = randomTime
 
             
-            print("modified value")
-            print(P[randomX][i])
+    #         # print("modified value")
+    #         # print(P[randomX][i])
 
     print("------------------------------------\n")
     print(P)
     return P
 
 
-# CityNumber = input("Please enter the number of cities that you want : ")
-# random_symmetric_matrix(CityNumber)
-random_symmetric_matrix(4)
+CityNumber = input("Please enter the number of cities that you want : ")
+random_symmetric_matrix(CityNumber)
+# random_symmetric_matrix(4)
